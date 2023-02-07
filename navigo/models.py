0b5efdff1da468e72bacda659ad1e1d1c87bb041
n01490224@humber.ca <n01490224@humber.ca>
2023-02-07 12:54:58 -0500

    n commit

--- navigo/models.py
+++ navigo/models.py
@@ -9,3 +9,4 @@ class Vehicle(models.Model):
 	vehicle_name = models.CharField(max_length=200)
 	vehicle_type = models.CharField(max_length=200)
 	date_added = models.DateTimeField(auto_now_add=True)
+	
